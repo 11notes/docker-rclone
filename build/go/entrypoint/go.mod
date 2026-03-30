5742910b91f212f0a9bec6a19e35f01b83fe6878
ElevenNotes <github@11notes.ch>
2026-03-30 15:27:21 +0200

    feat: switch to go-eleven

--- build/go/entrypoint/go.mod
+++ build/go/entrypoint/go.mod
@@ -1,3 +1,5 @@
 module github.com/11notes/docker-rclone/entrypoint
 go 1.25.0
-require github.com/11notes/docker-util v4.0.1+incompatible
+require (
+  github.com/11notes/go-eleven v0.3.0
+)
--- build/go/entrypoint/main.go
+++ build/go/entrypoint/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"os"
-	"syscall"
 
-	"github.com/11notes/docker-util"
+	"github.com/11notes/go-eleven"
 )
 
 func main(){
-	_ = eleven.Container.EnvToFile("RCLONE_CONFIG", "/rclone/etc/default.conf")	
-	if err := syscall.Exec("/usr/local/bin/rclone", eleven.Container.Command([]string{"rclone", "--rc", "--rc-addr", "0.0.0.0:5572", "--rc-enable-metrics", "--config", "/rclone/etc/default.conf"}), os.Environ()); err != nil {
-		os.Exit(1)
-	}
+	_ = eleven.Container.EnvToFile("RCLONE_CONFIG", "/rclone/etc/default.conf")
+	eleven.Container.Run("/usr/local/bin", "rclone", eleven.Container.Command([]string{"--rc", "--rc-addr", "0.0.0.0:5572", "--rc-enable-metrics", "--config", "/rclone/etc/default.conf"}))
 }
