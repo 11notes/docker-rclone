b9def70b3a6c3315bd692e794aaee2a7cc92ccaa
ElevenNotes <github@11notes.ch>
2026-07-09 14:39:59 +0200

    chore: update to 0.4.17

--- build/go/entrypoint/go.mod
+++ build/go/entrypoint/go.mod
@@ -1,5 +1,5 @@
 module github.com/11notes/docker-rclone/entrypoint
-go 1.25.0
+go 1.26.0
 require (
-  github.com/11notes/go-eleven v0.3.0
+  github.com/11notes/go-eleven v0.4.17
 )
--- build/go/entrypoint/main.go
+++ build/go/entrypoint/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"os"
-
 	"github.com/11notes/go-eleven"
 )
 
 func main(){
 	_ = eleven.Container.EnvToFile("RCLONE_CONFIG", "/rclone/etc/default.conf")
-	eleven.Container.Run("/usr/local/bin", "rclone", eleven.Container.Command([]string{"--rc", "--rc-addr", "0.0.0.0:5572", "--rc-enable-metrics", "--config", "/rclone/etc/default.conf"}))
+	eleven.Container.Run("/usr/local/bin", "rclone", eleven.Container.MergeCommand([]string{"--rc", "--rc-addr", "0.0.0.0:5572", "--rc-enable-metrics", "--config", "/rclone/etc/default.conf"}), []string{})
 }
